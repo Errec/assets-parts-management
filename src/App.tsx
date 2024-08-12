768cff26aa31df360c11acdbbe51e4f2988c48d7
Raniro Coelho <ranirocoelho@gmail.com>
2024-08-11 22:44:42 -0300

    Enable file drag and drop to tauri env

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-// App.tsx
 import React, { useEffect, useState } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import './App.css';
@@ -9,7 +8,7 @@ import { useCompanyStore } from './store/companyStore';
 
 const queryClient = new QueryClient();
 
-const App: React.FC = () => {
+const useCompanySelection = () => {
   const { fetchCompanies, error } = useCompanyStore();
   const [selectedCompanyId, setSelectedCompanyId] = useState<string | null>(null);
   const [searchTerm, setSearchTerm] = useState('');
@@ -18,9 +17,24 @@ const App: React.FC = () => {
     fetchCompanies();
   }, [fetchCompanies]);
 
-  const handleClearSearch = () => {
-    setSearchTerm('');
+  const handleClearSearch = () => setSearchTerm('');
+
+  return {
+    selectedCompanyId,
+    setSelectedCompanyId,
+    searchTerm,
+    handleClearSearch,
+    error,
   };
+};
+
+const App: React.FC = () => {
+  const {
+    selectedCompanyId,
+    setSelectedCompanyId,
+    handleClearSearch,
+    error,
+  } = useCompanySelection();
 
   if (error) {
     return <div>Error: {error}</div>;
@@ -30,16 +44,16 @@ const App: React.FC = () => {
     <QueryClientProvider client={queryClient}>
       <ErrorBoundary>
         <Header 
-          onSelectCompany={setSelectedCompanyId} 
+          onSelectCompany={setSelectedCompanyId}
           selectedCompanyId={selectedCompanyId} 
           onClearSearch={handleClearSearch}
         />
-        <main className="p-4 w-full flex items-center justify-center min-h-screen">
+        <main className={`p-4 w-full flex items-center justify-center min-h-screen ${!selectedCompanyId ? 'pointer-events-none' : ''}`}>
           {selectedCompanyId ? (
             <Container selectedCompanyId={selectedCompanyId} />
           ) : (
-            <div className="p-4 bg-blue-100 text-center">
-              <p className="text-blue-600">Select the unit</p>
+            <div className="p-4 bg-tractian-blue-200 text-center border-2 rounded-lg">
+              <p className="text-tractian-blue-50 text-2xl font-bold">Select the Unit</p>
             </div>
           )}
         </main>
